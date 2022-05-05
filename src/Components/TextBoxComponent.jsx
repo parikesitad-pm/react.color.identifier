3ea435d65da8f24411fae596a25af6669742a5d0
ParikesitAD <parikesit.ad@gmail.com>
2022-05-05 19:25:40 +0700

    minorAdjust

--- src/Components/TextBoxComponent.jsx
+++ src/Components/TextBoxComponent.jsx
@@ -2,7 +2,7 @@ const TextBox = ({ colorValue, hexValue }) => {
   return (
     <div className="card">
       <h5 className="uppercase font-bold text-slate-400">
-        {colorValue ? colorValue : ''}
+        {colorValue ? colorValue : 'please add pick a color'}
       </h5>
       <h5 className="uppercase text-neutral-400">
         {hexValue ? hexValue : null}
